23914b9db30622a6af9903ae22aac339a8be0a4c
ratanan vasunan <hattack2521@gmail.com>
2026-08-17 21:52:30 +0700

    Add Student3 and Student4 models

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, major ,Student1
+from .models import Student, major, Student1, Student3, Student4
 
 @admin.register(Student)
 class StdentsAdmin(admin.ModelAdmin):
@@ -13,5 +13,11 @@ class majorAdmin(admin.ModelAdmin):
 @admin.register(Student1)
 class Student1Admin(admin.ModelAdmin):
     list_display = ('st_id', 'prefix_name', 'first_name', 'last_name')
-    
 
+@admin.register(Student3)
+class Student3Admin(admin.ModelAdmin):
+    list_display = ('st_id', 'prefix_name', 'first_name', 'last_name')
+
+@admin.register(Student4)
+class Student4Admin(admin.ModelAdmin):
+    list_display = ('st_id', 'prefix_name', 'first_name', 'last_name')
