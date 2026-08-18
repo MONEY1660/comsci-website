c43d440c34caf53a97bbbe9e4a19dd0ffa4d1c80
ratanan vasunan <hattack2521@gmail.com>
2026-08-19 02:12:15 +0700

    fix14

--- app1/urls.py
+++ app1/urls.py
@@ -6,21 +6,87 @@ app_name = 'app1'
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
 
-    path('dashboard/', views.dashboard, name='dashboard'),
+    # ==============================
+    # หน้าเว็บไซต์หลัก
+    # ==============================
 
-    path('about/', views.about, name='about'),
+    path(
+        '',
+        views.index,
+        name='home'
+    ),
 
-    path('contact/', views.contact, name='contact'),
+    path(
+        'dashboard/',
+        views.dashboard,
+        name='dashboard'
+    ),
 
-    # รายชื่อนักศึกษา
-    path('students/', views.students, name='students'),
+    path(
+        'about/',
+        views.about,
+        name='about'
+    ),
 
-    # รายละเอียดนักศึกษา
+    path(
+        'contact/',
+        views.contact,
+        name='contact'
+    ),
+
+
+    # ==============================
+    # ข้อมูลนักศึกษาทั้ง 4 ชั้นปี
+    # ==============================
+
+    path(
+        'students/',
+        views.students,
+        name='students'
+    ),
+
+
+    # ==============================
+    # รายละเอียดนักศึกษาปี 1
+    # ==============================
+
+    path(
+        'student1/<str:pk>/',
+        views.student_detail1,
+        name='student_detail1'
+    ),
+
+
+    # ==============================
+    # รายละเอียดนักศึกษาปี 2
+    # ==============================
+
     path(
         'student/<str:pk>/',
-        views.student_detail,
-        name='student_detail'
+        views.student_detail2,
+        name='student_detail2'
+    ),
+
+
+    # ==============================
+    # รายละเอียดนักศึกษาปี 3
+    # ==============================
+
+    path(
+        'student3/<str:pk>/',
+        views.student_detail3,
+        name='student_detail3'
+    ),
+
+
+    # ==============================
+    # รายละเอียดนักศึกษาปี 4
+    # ==============================
+
+    path(
+        'student4/<str:pk>/',
+        views.student_detail4,
+        name='student_detail4'
     ),
 ]
